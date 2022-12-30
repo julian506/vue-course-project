b370048a10f68ae8d577321b38478f8be24f8c98
Julián Pachón Castrillón <jupachon@unal.edu.co>
2022-12-30 10:57:51 -0500

    Installed material design icons

--- project/src/main.js
+++ project/src/main.js
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import "@mdi/font/css/materialdesignicons.min.css";
 
 const vuetify = createVuetify({
   components,
