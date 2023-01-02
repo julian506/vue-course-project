89ebcaf3ba3d47cb2490f39fabc995b3983558b1
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-01-02 13:12:33 -0500

    Created responsive register form

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,91 +1,137 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+// import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <v-app>
+    <v-layout>
+      <v-app-bar :elevation="2">
+        <v-app-bar-title>Proyecto de prueba</v-app-bar-title>
+      </v-app-bar>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <v-main class="bg-indigo-lighten-4">
+        <v-content>
+          <v-container>
+            <v-row justify="center">
+              <v-col xs="12" sm="6" md="4">
+                <v-form>
+                  <v-card
+                    variant="outlined"
+                    class="mx-auto pa-5 pb-8 bg-white mt-5"
+                    elevation="8"
+                    rounded="lg"
+                  >
+                    <v-row justify="center">
+                      <v-col cols="12" sm="6" md="8">
+                        <v-card-item>
+                          <v-card-title class="text-center"
+                            >Formulario de registro</v-card-title
+                          >
+                        </v-card-item>
+                      </v-col>
+                    </v-row>
+                    <v-card-text>
+                      <v-row justify="center">
+                        <v-col cols="12" sm="6" md="8">
+                          <v-text-field
+                            variant="outlined"
+                            label="Nombre"
+                            v-model="nombre"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row justify="center">
+                        <v-col cols="12" sm="6" md="8">
+                          <v-text-field
+                            variant="outlined"
+                            label="Correo"
+                            type="email"
+                            v-model="correo"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row justify="center">
+                        <v-col cols="12" sm="6" md="8">
+                          <v-text-field
+                            variant="outlined"
+                            label="Edad"
+                            type="number"
+                            v-model="edad"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row justify="center">
+                        <v-col cols="12" sm="6" md="8">
+                          <v-select
+                            label="Estado civil"
+                            :items="['Soltero/a', 'Casado/a', 'Viudo/a']"
+                            variant="outlined"
+                            v-model="estadoCivil"
+                          ></v-select>
+                        </v-col>
+                      </v-row>
+                      <v-row justify="center">
+                        <v-col cols="12" sm="6" md="8">
+                          <v-text-field
+                            variant="outlined"
+                            label="Pasatiempo"
+                            type="text"
+                            v-model="pasatiempo"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row justify="center">
+                        <v-col cols="12" sm="6" md="8">
+                          <v-btn
+                            block
+                            prepend-icon="mdi-send-circle"
+                            variant="outlined"
+                            color="primary"
+                          >
+                            Registrarse
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
+                </v-form>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-content>
+      </v-main>
+    </v-layout>
+    <v-footer class="bg-indigo-lighten-1 text-center d-flex flex-column">
+      <div class="pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+        Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+        accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a
+        sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+        lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+        iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor
+        vel ut orci. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus.
+      </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <v-divider></v-divider>
 
-  <RouterView />
+      <div>{{ new Date().getFullYear() }} — <strong>Vuetify</strong></div>
+    </v-footer>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      nombre: null,
+      correo: null,
+      edad: null,
+      estadoCivil: null,
+      pasatiempo: null,
+    };
+  },
+};
+</script>
