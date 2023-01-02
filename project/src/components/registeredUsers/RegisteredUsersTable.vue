e11cf9658affb1313e61345a95793e703dcb6401
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-01-02 15:52:48 -0500

    Created registered users view and table component using props to communicate the main component with the table component using props

---
+++ project/src/components/registeredUsers/RegisteredUsersTable.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+<template>
+  <v-container grid-list-xs>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">Nombre</th>
+          <th class="text-left">Email</th>
+          <th class="text-left">Edad</th>
+          <th class="text-left">Estado Civil</th>
+          <th class="text-left">Pasatiempo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.name">
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.age }}</td>
+          <td>{{ user.maritalStatus }}</td>
+          <td>{{ user.hobby }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "RegisteredUsersTable",
+  props: {
+    users: Array,
+  },
+};
+</script>
---
+++ project/src/views/RegisteredUsersView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import registeredUsersTable from "../components/registeredUsers/RegisteredUsersTable.vue";
+</script>
+<template>
+  <registeredUsersTable :users="users"></registeredUsersTable>
+</template>
+<script>
+export default {
+  props: {
+    users: Array,
+  },
+};
+</script>
