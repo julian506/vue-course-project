28586f338c8d2dab71f8424f7d931047aaccd848
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-01-02 15:52:02 -0500

    Created methods to capture when a user is registered using the register form

--- project/src/App.vue
+++ project/src/App.vue
@@ -4,6 +4,7 @@
 import NavBar from "./components/NavBar.vue";
 import Footer from "./components/MainFooter.vue";
 import RegisterView from "./views/RegisterView.vue";
+import RegisteredUsersView from "./views/RegisteredUsersView.vue";
 </script>
 
 <template>
@@ -12,8 +13,24 @@ import RegisterView from "./views/RegisterView.vue";
       <NavBar> </NavBar>
       <v-main class="bg-indigo-lighten-4">
         <RegisterView @userCreated="addUser"></RegisterView>
+        <RegisteredUsersView :users="users"></RegisteredUsersView>
       </v-main>
     </v-layout>
     <Footer></Footer>
   </v-app>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    addUser(e) {
+      this.users.push(e);
+      console.log(this.users);
+    },
+  },
+};
+</script>
--- project/src/components/RegisterForm.vue
+++ project/src/components/RegisterForm.vue
@@ -1,95 +1,92 @@
 <template>
-  <v-main class="bg-indigo-lighten-4">
-    <v-content>
-      <v-container>
-        <v-row justify="center">
-          <v-col xs="12" sm="6" md="4">
-            <v-form>
-              <v-card
-                variant="outlined"
-                class="mx-auto pa-5 pb-8 bg-white mt-5"
-                elevation="8"
-                rounded="lg"
-              >
-                <v-row justify="center">
-                  <v-col cols="12" sm="6" md="8">
-                    <v-card-item>
-                      <v-card-title class="text-center"
-                        >Formulario de registro</v-card-title
-                      >
-                    </v-card-item>
-                  </v-col>
-                </v-row>
-                <v-card-text>
-                  <v-row justify="center">
-                    <v-col cols="12" sm="6" md="8">
-                      <v-text-field
-                        variant="outlined"
-                        label="Nombre"
+  <v-container>
+    <v-row justify="center">
+      <v-col xs="12" sm="6" md="4">
+        <v-form>
+          <v-card
+            variant="outlined"
+            class="mx-auto pa-5 pb-8 bg-white mt-5"
+            elevation="8"
+            rounded="lg"
+          >
+            <v-row justify="center">
+              <v-col cols="12" sm="6" md="8">
+                <v-card-item>
+                  <v-card-title class="text-center"
+                    >Formulario de registro</v-card-title
+                  >
+                </v-card-item>
+              </v-col>
+            </v-row>
+            <v-card-text>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8">
+                  <v-text-field
+                    variant="outlined"
+                    label="Nombre"
                     v-model="name"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <v-row justify="center">
-                    <v-col cols="12" sm="6" md="8">
-                      <v-text-field
-                        variant="outlined"
-                        label="Correo"
-                        type="email"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8">
+                  <v-text-field
+                    variant="outlined"
+                    label="Correo"
+                    type="email"
                     v-model="email"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <v-row justify="center">
-                    <v-col cols="12" sm="6" md="8">
-                      <v-text-field
-                        variant="outlined"
-                        label="Edad"
-                        type="number"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8">
+                  <v-text-field
+                    variant="outlined"
+                    label="Edad"
+                    type="number"
                     v-model="age"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <v-row justify="center">
-                    <v-col cols="12" sm="6" md="8">
-                      <v-select
-                        label="Estado civil"
-                        :items="['Soltero/a', 'Casado/a', 'Viudo/a']"
-                        variant="outlined"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8">
+                  <v-select
+                    label="Estado civil"
+                    :items="['Soltero/a', 'Casado/a', 'Viudo/a']"
+                    variant="outlined"
                     v-model="maritalStatus"
-                      ></v-select>
-                    </v-col>
-                  </v-row>
-                  <v-row justify="center">
-                    <v-col cols="12" sm="6" md="8">
-                      <v-text-field
-                        variant="outlined"
-                        label="Pasatiempo"
-                        type="text"
+                  ></v-select>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8">
+                  <v-text-field
+                    variant="outlined"
+                    label="Pasatiempo"
+                    type="text"
                     v-model="hobby"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <v-row justify="center">
-                    <v-col cols="12" sm="6" md="8">
-                      <v-btn
-                        block
-                        prepend-icon="mdi-send-circle"
-                        variant="outlined"
-                        color="primary"
-                      >
-                        Registrarse
-                      </v-btn>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-              </v-card>
-            </v-form>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-main>
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="8">
+                  <v-btn
+                    block
+                    prepend-icon="mdi-send-circle"
+                    variant="outlined"
+                    color="primary"
+                    @click="saveInformation"
+                  >
+                    Registrarse
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
@@ -101,8 +98,20 @@ export default {
       age: null,
       maritalStatus: null,
       hobby: null,
-      users: [],
     };
+  },
+  methods: {
+    saveInformation() {
+      let newUser = {
+        name: this.name,
+        email: this.email,
+        age: this.age,
+        maritalStatus: this.maritalStatus,
+        hobby: this.hobby,
+      }
+      alert("Usuario creado con éxito");
+      this.$emit("userCreated", newUser);
+    },
   },
 };
 </script>
